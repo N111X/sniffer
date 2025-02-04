980732b5c58563cd878b4f128dc3303b0d1bdf26
NIX <n11ixxor64@gmail.com>
2025-02-04 16:08:06 -0600

    main.py

--- main.py
+++ main.py
@@ -4,65 +4,54 @@ import argparse
 
 init(autoreset=True)
 
-
 def main(interface, pcapfile, export, protocol, text, display):
-    # Crea una instancia de la clase Sniffer
     sniffer = Sniffer()
 
-    # Si se proporciona un archivo .pcap, lo importamos
+    # Importar captura desde archivo o iniciar captura en vivo
     if pcapfile:
         print(Fore.GREEN + f"[+] Importando archivo: {pcapfile}")
         sniffer.import_capture(pcapfile)
-
-    # Si no se proporciona un archivo .pcap, comenzamos a capturar en vivo
     else:
         print(Fore.GREEN + f"[+] Comenzando captura en vivo en la interfaz: {interface}")
         sniffer.capture_live(interface=interface)
 
-    # Filtrar los paquetes si se ha proporcionado un protocolo
+    # Aplicar filtros si se proporcionan
+    filtered_packets = sniffer.capture_packet
+
     if protocol:
         print(Fore.YELLOW + f"[+] Filtrando por protocolo: {protocol}")
         filtered_packets = sniffer.filter_by_protocol(protocol)
-        sniffer.print_packets(filtered_packets)
 
-    # Filtrar los paquetes si se ha proporcionado un texto
     if text:
         print(Fore.YELLOW + f"[+] Filtrando por texto: {text}")
         filtered_packets = sniffer.filter_by_text(text)
+
+    # Mostrar paquetes filtrados o capturados
+    if display:
+        print(Fore.CYAN + "[+] Mostrando paquetes:")
         sniffer.print_packets(filtered_packets)
 
-    # Si se ha solicitado exportar a un archivo .pcap
+    # Exportar los paquetes filtrados o capturados
     if export:
         print(Fore.GREEN + f"[+] Exportando paquetes a: {export}")
-        sniffer.export_to_pcap(sniffer.capture_packet, export)
-
-    # Si se solicita mostrar los paquetes capturados
-    if display:
-        print(Fore.CYAN + "[+] Mostrando paquetes capturados:")
-        sniffer.print_packets()
-
+        sniffer.export_to_pcap(filtered_packets, export)
 
 if __name__ == '__main__':
-    # Impresion y configuracion de los argumentos
     parser = argparse.ArgumentParser(
         description=f'{Fore.LIGHTBLUE_EX}Desarrollado por: N111X\n{Fore.LIGHTBLUE_EX}Sniffer.',
-        formatter_class=argparse.RawTextHelpFormatter,  # Usamos RawTextHelpFormatter para controlar el formato
+        formatter_class=argparse.RawTextHelpFormatter,
         add_help=True
     )
 
-    # Argumentos que puede recibir el script
-    parser.add_argument("-i", "--interface", help="Interfaz de red para la captura en vivo", type=str,
-                        default="Ethernet")
+    parser.add_argument("-i", "--interface", help="Interfaz de red para la captura en vivo", type=str, default="Ethernet")
     parser.add_argument("-r", "--read-pcap", help="Importar un archivo .pcap para su analisis", type=str)
-    parser.add_argument("-e", "--export-pcap", help="Exportar el analisis en un fichero .pcap", type=str,
-                        default="capture.pcap")
+    parser.add_argument("-e", "--export-pcap", help="Exportar el analisis en un fichero .pcap", type=str)
     parser.add_argument("-p", "--protocol", help="Filtrar paquetes por un protocolo especifico", type=str)
     parser.add_argument("-t", "--text", help="Filtrar paquetes que contengan un texto especifico", type=str)
     parser.add_argument("-d", "--display", help="Mostrar los paquetes en la terminal", action="store_true")
 
     args = parser.parse_args()
 
-    # Llamamos a la funcion main con los argumentos parseados
     main(
         interface=args.interface,
         pcapfile=args.read_pcap,
